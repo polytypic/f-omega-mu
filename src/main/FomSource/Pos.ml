9ba9d138a518ec6404f168d88140f09966366843
Vesa Karvonen <vesa.a.j.k@gmail.com>
2021-06-30 20:47:31 +0300

    Order first by line number

--- src/main/FomSource/Pos.ml
+++ src/main/FomSource/Pos.ml
@@ -6,6 +6,6 @@ let of_path path = {Lexing.dummy_pos with pos_fname = path}
 let column_of {Lexing.pos_cnum; pos_bol; _} = pos_cnum - pos_bol + 1
 
 let compare (l : t) (r : t) =
-  String.compare l.pos_fname r.pos_fname <>? fun () ->
   Int.compare l.pos_lnum r.pos_lnum <>? fun () ->
-  Int.compare l.pos_cnum r.pos_cnum
+  Int.compare l.pos_cnum r.pos_cnum <>? fun () ->
+  String.compare l.pos_fname r.pos_fname
