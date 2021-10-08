83638f74732e3ff76a8b3be0c807f969d01770e0
Vesa Karvonen <vesa.a.j.k@gmail.com>
2021-10-08 23:09:59 +0300

    Restructure code for clarity

--- src/main/FomParser/Lexer.ml
+++ src/main/FomParser/Lexer.ml
@@ -183,91 +183,91 @@ type t = Buffer.t -> unit -> tok
 
 let plain : t = fun buffer () -> token buffer
 
-type token_info = {begins : int; ends : int; name : string}
-
-let atom = "atom"
-let builtin = "type"
-let comment = "comment"
-let error = "error"
-let keyword = "keyword"
-let number = "number"
-let operator = "operator"
-let punctuation = "punctuation"
-let string = "string"
-let tag = "tag"
-let variable = "variable"
+(* *)
 
 let offset_as_utf_16 input i =
   let input = UTF.UTF8.to_uchar_array input in
   (Array.sub input 0 i |> UTF.UTF16.of_uchar_array |> Bytes.length) / 2
 
-let token_info_utf_8 input =
-  Buffer.from_utf_8 input |> token_or_comment |> fun (token, lhs, rhs) ->
-  {
-    begins = lhs.pos_cnum;
-    ends = rhs.pos_cnum;
-    name =
-      (match token with
-      | And -> keyword
-      | ArrowRight -> operator
-      | Backslash -> punctuation
-      | BraceLhs -> punctuation
-      | BraceRhs -> punctuation
-      | BracketLhs -> punctuation
-      | BracketRhs -> punctuation
-      | Caret -> operator
-      | Case -> keyword
-      | Colon -> punctuation
-      | Comma -> punctuation
-      | Comment _ -> comment
-      | Diamond -> punctuation
-      | Dot -> punctuation
-      | DoubleAngleLhs -> punctuation
-      | DoubleAngleRhs -> punctuation
-      | EOF -> error
-      | Else -> keyword
-      | Equal -> operator
-      | Exists -> tag
-      | ForAll -> tag
-      | Greater -> operator
-      | GreaterEqual -> operator
-      | Id ("bool" | "int" | "impure" | "string") -> builtin
-      | Id ("true" | "false") -> atom
-      | Id _ -> variable
-      | IdTyp _ -> variable
-      | IdSub _ -> variable
-      | If -> keyword
-      | Import -> keyword
-      | In -> keyword
-      | Include -> keyword
-      | LambdaLower -> tag
-      | LambdaUpper -> tag
-      | Less -> operator
-      | LessEqual -> operator
-      | Let -> keyword
-      | LitNat _ -> number
-      | LitString _ -> string
-      | LogicalAnd -> operator
-      | LogicalNot -> operator
-      | LogicalOr -> operator
-      | Minus -> operator
-      | MuLower -> tag
-      | NotEqual -> operator
-      | ParenLhs -> punctuation
-      | ParenRhs -> punctuation
-      | Percent -> operator
-      | Pipe -> punctuation
-      | Plus -> operator
-      | Slash -> operator
-      | Star -> operator
-      | Target -> keyword
-      | Then -> keyword
-      | Tick -> punctuation
-      | TriangleLhs -> punctuation
-      | TriangleRhs -> punctuation
-      | Type -> keyword
-      | Underscore -> variable);
-  }
+(* *)
+
+type token_info = {begins : int; ends : int; name : string}
+
+let token_info_utf_8 =
+  let atom = "atom"
+  and builtin = "type"
+  and comment = "comment"
+  and error = "error"
+  and keyword = "keyword"
+  and number = "number"
+  and operator = "operator"
+  and punctuation = "punctuation"
+  and string = "string"
+  and tag = "tag"
+  and variable = "variable" in
+  let to_name = function
+    | And -> keyword
+    | ArrowRight -> operator
+    | Backslash -> punctuation
+    | BraceLhs -> punctuation
+    | BraceRhs -> punctuation
+    | BracketLhs -> punctuation
+    | BracketRhs -> punctuation
+    | Caret -> operator
+    | Case -> keyword
+    | Colon -> punctuation
+    | Comma -> punctuation
+    | Comment _ -> comment
+    | Diamond -> punctuation
+    | Dot -> punctuation
+    | DoubleAngleLhs -> punctuation
+    | DoubleAngleRhs -> punctuation
+    | EOF -> error
+    | Else -> keyword
+    | Equal -> operator
+    | Exists -> tag
+    | ForAll -> tag
+    | Greater -> operator
+    | GreaterEqual -> operator
+    | Id ("bool" | "int" | "impure" | "string") -> builtin
+    | Id ("true" | "false") -> atom
+    | Id _ -> variable
+    | IdTyp _ -> variable
+    | IdSub _ -> variable
+    | If -> keyword
+    | Import -> keyword
+    | In -> keyword
+    | Include -> keyword
+    | LambdaLower -> tag
+    | LambdaUpper -> tag
+    | Less -> operator
+    | LessEqual -> operator
+    | Let -> keyword
+    | LitNat _ -> number
+    | LitString _ -> string
+    | LogicalAnd -> operator
+    | LogicalNot -> operator
+    | LogicalOr -> operator
+    | Minus -> operator
+    | MuLower -> tag
+    | NotEqual -> operator
+    | ParenLhs -> punctuation
+    | ParenRhs -> punctuation
+    | Percent -> operator
+    | Pipe -> punctuation
+    | Plus -> operator
+    | Slash -> operator
+    | Star -> operator
+    | Target -> keyword
+    | Then -> keyword
+    | Tick -> punctuation
+    | TriangleLhs -> punctuation
+    | TriangleRhs -> punctuation
+    | Type -> keyword
+    | Underscore -> variable
+  in
+  Buffer.from_utf_8 >>> token_or_comment >>> fun (token, lhs, rhs) ->
+  {begins = lhs.pos_cnum; ends = rhs.pos_cnum; name = to_name token}
 
 let[@warning "-32"] to_string = function
   | And -> "and"
