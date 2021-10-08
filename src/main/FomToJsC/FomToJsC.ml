fa743c59393808d7aebe74784ba938d211be216f
Vesa Karvonen <vesa.a.j.k@gmail.com>
2021-10-09 00:51:21 +0300

    Avoid unnecessary binding

--- src/main/FomToJsC/FomToJsC.ml
+++ src/main/FomToJsC/FomToJsC.ml
@@ -66,18 +66,15 @@ let mods_in_js : (string, (Zero.t, Cats.t) IVar.t) Hashtbl.t =
 let compile_to_js_all paths =
   paths |> erase_and_simplify_all
   >>= List.map_fr @@ fun (id, path, erased) ->
-      let+ js =
-        match Hashtbl.find_opt mods_in_js path with
-        | Some var -> IVar.get var |> generalize_error
-        | None ->
-          let var = IVar.empty () in
-          Hashtbl.replace mods_in_js path var;
-          erased
-          |> FomToJs.to_js ~top:(`Const id)
-          >>- (fun js -> str "// " ^ str path ^ str "\n" ^ js)
-          |> catch >>= IVar.put var >> IVar.get var |> generalize_error
-      in
-      js
+      match Hashtbl.find_opt mods_in_js path with
+      | Some var -> IVar.get var |> generalize_error
+      | None ->
+        let var = IVar.empty () in
+        Hashtbl.replace mods_in_js path var;
+        erased
+        |> FomToJs.to_js ~top:(`Const id)
+        >>- (fun js -> str "// " ^ str path ^ str "\n" ^ js)
+        |> catch >>= IVar.put var >> IVar.get var |> generalize_error
 
 let modules_to_js ast paths =
   let* paths = topological_deps paths in
