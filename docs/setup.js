35b10dfaae4863a3bbfc72d3f4dd11e2aa79f6c8
Vesa Karvonen <vesa.a.j.k@gmail.com>
2021-10-23 20:05:14 +0300

    Greek letter escapes

--- docs/setup.js
+++ docs/setup.js
@@ -448,6 +448,37 @@ const alternatives = {}
 for (const {unicode, ascii, bop} of fom.synonyms())
   alternatives[ascii] = unicode + (bop ? ' ' : '')
 
+for (const [basename, upper, lower, alternate] of [
+  ['Alpha', 'A', 'α'],
+  ['Beta', 'B', 'β'],
+  ['Gamma', 'Γ', 'γ'],
+  ['Delta', 'Δ', 'δ'],
+  ['Epsilon', 'E', 'ϵ', 'ε'],
+  ['Zeta', 'Ζ', 'ζ'],
+  ['Eta', 'Η', 'η'],
+  ['Theta', 'Θ', 'θ', 'ϑ'],
+  ['Iota', 'Ι', 'ι'],
+  ['Kappa', 'Κ', 'κ', 'ϰ'],
+  ['Lambda', 'Λ', 'λ'],
+  ['Mu', 'Μ', 'μ'],
+  ['Nu', 'Ν', 'ν'],
+  ['Xi', 'Ξ', 'ξ'],
+  ['Omicron', 'O', 'ℴ'],
+  ['Pi', 'Π', 'π', 'ϖ'],
+  ['Rho', 'Ρ', 'ρ', 'ϱ'],
+  ['Sigma', 'Σ', 'σ', 'ς'],
+  ['Tau', 'Τ', 'τ'],
+  ['Upsilon', 'ϒ', 'υ'],
+  ['Phi', 'Φ', 'ϕ', 'φ'],
+  ['Chi', 'X', 'χ'],
+  ['Psi', 'Ψ', 'ψ'],
+  ['Omega', 'Ω', 'ω'],
+]) {
+  alternatives[`\\${basename}`] = upper
+  alternatives[`\\${basename.toLowerCase()}`] = lower
+  if (alternate) alternatives[`\\var${basename.toLowerCase()}`] = alternate
+}
+
 //
 
 fomCM.on('keyup', (_, event) => {
