e13c9fa5d2591c5f5b90a8d9798a5b4dea8689fb
Vesa Karvonen <vesa.a.j.k@gmail.com>
2021-10-25 14:31:25 +0300

    Try to highlight record labels

--- docs/setup.js
+++ docs/setup.js
@@ -78,20 +78,46 @@ const cmConfig = {
 
 const jsCM = CodeMirror(jsDiv, {...cmConfig, mode: 'javascript'})
 
+const nextNonSpace = (str, i, dir = 1) => {
+  while (0 <= i && i <= str.length) {
+    const c = str[i]
+    if (c !== ' ' && c !== '\t' && c !== '\n') return c
+    i += dir
+  }
+  return ''
+}
+
 CodeMirror.defineMode('fom', () => ({
-  startState: () => ({state: fom.initial}),
+  startState: () => ({previous: '', state: fom.initial}),
   token: (stream, state) => {
+    const start = stream.start
+
     const input = stream.string.slice(stream.start)
     const token = fom.token(input, state.state)
+
     state.state = token.state
+
     if (token.name === 'error') {
       stream.skipToEnd()
     } else {
       stream.start += fom.offset16(input, token.begins)
       stream.pos += fom.offset16(input, token.ends)
     }
-    if (token.name === 'variable' && stream.string[stream.start - 1] === "'")
-      return 'property'
+
+    if (
+      token.name === 'variable' &&
+      (nextNonSpace(stream.string, stream.start - 1, -1) === "'" ||
+        ((nextNonSpace(stream.string, stream.pos) === ':' ||
+          nextNonSpace(stream.string, stream.pos) === '=') &&
+          (state.previous === 'punctuation' ||
+            (stream.start &&
+              !nextNonSpace(stream.string, stream.start - 1, -1)))))
+    ) {
+      token.name = 'property'
+    }
+
+    state.previous = token.name
+
     return token.name
   },
 }))
