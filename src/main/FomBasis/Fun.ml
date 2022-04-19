f483e84167c802e032d76ed37c1573add7a428d3
Vesa Karvonen <vesa.a.j.k@gmail.com>
2022-04-19 22:49:17 +0300

    Add `Fun.Syntax.uncurry`

--- src/main/FomBasis/Fun.ml
+++ src/main/FomBasis/Fun.ml
@@ -3,6 +3,7 @@ include Stdlib.Fun
 module Syntax = struct
   let id = id
   let const = const
+  let uncurry abc (a, b) = abc a b
   let ( >>> ) ab bc a = bc (ab a)
   let ( <<< ) bc ab a = bc (ab a)
 end
--- src/main/FomBasis/Fun.mli
+++ src/main/FomBasis/Fun.mli
@@ -3,6 +3,7 @@ include module type of Stdlib.Fun
 module Syntax : sig
   val id : 'a -> 'a
   val const : 'a -> 'b -> 'a
+  val uncurry : ('a -> 'b -> 'c) -> 'a * 'b -> 'c
   val ( >>> ) : ('a -> 'b) -> ('b -> 'c) -> 'a -> 'c
   val ( <<< ) : ('b -> 'c) -> ('a -> 'b) -> 'a -> 'c
 end
