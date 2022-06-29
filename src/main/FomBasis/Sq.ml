3f9d54afbb1df2102058a65918953f2b2ee57a61
Vesa Karvonen <vesa.a.j.k@gmail.com>
2022-06-29 17:38:08 +0300

    Add `Sq.map_eq_fr`

--- src/main/FomBasis/Sq.ml
+++ src/main/FomBasis/Sq.ml
@@ -1,6 +1,11 @@
+open Functor.Syntax
 open Applicative.Syntax
 
 type 'a t = 'a * 'a
 
 let exists pr (x, y) = pr x || pr y
 let map_fr fn (x, y) = fn x <*> fn y
+
+let map_eq_fr fn ((x, y) as xy) =
+  fn x <*> fn y >>- fun ((x', y') as xy') ->
+  if x == x' && y == y' then xy else xy'
--- src/main/FomBasis/Sq.mli
+++ src/main/FomBasis/Sq.mli
@@ -4,3 +4,6 @@ val exists : ('a -> bool) -> 'a t -> bool
 
 val map_fr :
   ('a -> ('f, 'F, 'b) Applicative.fr) -> 'a t -> ('f, 'F, 'b t) Applicative.fr
+
+val map_eq_fr :
+  ('a -> ('f, 'F, 'a) Applicative.fr) -> 'a t -> ('f, 'F, 'a t) Applicative.fr
